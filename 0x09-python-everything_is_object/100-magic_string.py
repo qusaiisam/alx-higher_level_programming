5b65233d8e602b0bd03764da97959139dc9c4393
qusaiisam <qusaiisam@gmail.com>
2024-01-05 23:11:04 -0800

    Everything is object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(repetitions=[-1]):
-    repetitions[0] += 1
-    return "Holberton, " * repetitions[0] + "Holberton"
+def magic_string():
+    magic_string.count = getattr(magic_string, 'count', 0) + 1
+    return ", ".join(["BestSchool" for i in range(magic_string.count)])
